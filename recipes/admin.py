748a7b25206ed19bdb889b0e8cbd0a6bdaf40975
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-06-20 17:38:01 +0300

    Add basic code for unit of measurement model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, Tag
+from .models import Ingredient, Recipe, Tag, UnitOfMeasurement
 
 
 class IngredientAdmin(admin.ModelAdmin):
@@ -23,6 +23,12 @@ class TagAdmin(admin.ModelAdmin):
     ordering = ('name',)
 
 
+class UnitOfMeasurementAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(UnitOfMeasurement, UnitOfMeasurementAdmin)
--- recipes/models/__init__.py
+++ recipes/models/__init__.py
@@ -1,3 +1,3 @@
-from .ingredient_model import Ingredient  # noqa
+from .ingredient_model import Ingredient, UnitOfMeasurement  # noqa
 from .recipe_model import Recipe  # noqa
 from .tag_model import Tag  # noqa
