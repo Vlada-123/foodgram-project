4b2d46eda4e6116c1e78efa56f3d2bcaef33e687
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-06-22 17:44:10 +0300

    Prettify urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,10 +3,17 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('favorites/', views.favorites, name='favorites'),
-    path('recipes/new/', views.create_recipe, name='create_recipe'),
-    path('recipes/<int:user_id>/<slug:slug>/', views.recipe_details,
+    path('',
+         views.home,
+         name='home'),
+    path('favorites/',
+         views.favorites,
+         name='favorites'),
+    path('recipes/new/',
+         views.create_recipe,
+         name='create_recipe'),
+    path('recipes/<int:user_id>/<slug:slug>/',
+         views.recipe_details,
          name='recipe_details'),
     path('recipes/<int:user_id>/<slug:slug>/delete/',
          views.delete_recipe,
@@ -14,5 +21,7 @@ urlpatterns = [
     path('recipes/<int:user_id>/<slug:slug>/edit/',
          views.edit_recipe,
          name='edit_recipe'),
-    path('users/<int:user_id>/', views.profile, name='profile'),
+    path('users/<int:user_id>/',
+         views.profile,
+         name='profile'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,17 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('register/', views.SignUp.as_view(), name='register'),
-    path('subscriptions/active/', views.subscriptions, name='subscriptions'),
-    path('subscriptions/', views.add_subscription, name='add_subscription'),
+    path('',
+         include('django.contrib.auth.urls')),
+    path('register/',
+         views.SignUp.as_view(),
+         name='register'),
+    path('subscriptions/active/',
+         views.subscriptions,
+         name='subscriptions'),
+    path('subscriptions/',
+         views.add_subscription,
+         name='add_subscription'),
     path('subscriptions/<int:user_id>/',
          views.remove_subscription,
          name='remove_subscription')
