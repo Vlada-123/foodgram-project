13fbd1ffdaf3343c5cf3cd29da1993d6b7ca0146
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-06-26 21:40:15 +0300

    Add default settings for emails

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -129,6 +129,12 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 RECORDS_ON_PAGE = 10
 
