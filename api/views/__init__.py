9bbcf79ac70a61976827efd0eb3e779536ce7c12
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-06-27 18:43:44 +0300

    Optimize mixin usage (after 1st review)

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -2,3 +2,4 @@ from .favorite_view import FavoriteViewSet  # noqa
 from .ingredient_view import IngredientViewSet  # noqa
 from .recipe_view import RecipeViewSet  # noqa
 from .user_view import UserViewSet  # noqa
+from .views_mixins import CreateDestroyViewSet  # noqa
