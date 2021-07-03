ebe2428206fc65117237bf6b4e1e223332402230
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-07-03 18:28:10 +0300

    Update flake8 setup

--- api/serializers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .favorite_serializer import FavoriteSerializer  # noqa
-from .ingredient_serializer import IngredientSerializer  # noqa
-from .recipe_serializer import RecipeSerializer  # noqa
-from .user_serializer import UserSerializer  # noqa
--- api/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .favorite_view import FavoriteViewSet  # noqa
-from .ingredient_view import IngredientViewSet  # noqa
-from .recipe_view import RecipeViewSet  # noqa
-from .user_view import UserViewSet  # noqa
-from .views_mixins import CreateDestroyViewSet  # noqa
